066c253bf1b3e9cca9f83b07e25914ce83cf42dc
hoehooiyan <hoehooiyan@gmail.com>
2020-03-01 22:23:48 +0800

    Add needed fontawesome icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import './fontawesome'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+    </div>
+  )
 }
 
 export default App
---
+++ src/fontawesome.js
@@ -0,0 +1,9 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import {
+  faMoon,
+  faSearch,
+  faChevronDown,
+  faArrowLeft
+} from '@fortawesome/free-solid-svg-icons'
+library.add(faMoon, faSearch, faChevronDown, faArrowLeft)
