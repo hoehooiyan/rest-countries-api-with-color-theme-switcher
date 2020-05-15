e09b10ecd57b7c62c4ea50b5b9817e1d55d8adeb
hoehooiyan <hoehooiyan@gmail.com>
2020-05-15 22:33:17 +0800

    :art: Destructure App.js into smaller component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Home from '../pages/home'
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+    </Switch>
+  )
+}
+
+export default App
