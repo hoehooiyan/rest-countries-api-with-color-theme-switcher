adc99b80b4ba7b0f27fea8158d0b9ddf892a736a
hoehooiyan <hoehooiyan@gmail.com>
2020-05-15 22:35:02 +0800

    :iphone: Work on responsive design
    For SearchBar and FilterRegion components

---
+++ src/components/FilterRegion.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styled from 'styled-components'
+
+export const StyledFilterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-weight: var(--normal);
+  position: relative;
+`
+
+export const StyledTopContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${(props) => props.theme.element};
+  border-radius: var(--radius);
+  box-shadow: var(--shadow);
+  color: ${(props) => props.theme.text};
+  cursor: pointer;
+  font-weight: var(--normal);
+  height: 4.8rem;
+  margin: 0 15.9rem 0.4rem 1.6rem;
+  padding: 1.6rem 1.9rem 1.6rem 2.4rem;
+  width: 20rem;
+
+  @media screen and (min-width: 768px) {
+    margin: 0 4rem 0.4rem 1.6rem;
+  }
+
+  @media screen and (min-width: 1440px) {
+    margin-right: 8rem;
+  }
+
+  .icon {
+    font-size: 10px;
+  }
+`
+
+export const StyledText = styled.p`
+  @media screen and (min-width: 768px) {
+    font-size: var(--mdFont);
+  }
+`
+
+export const StyledDropdown = styled.div`
+  background-color: ${(props) => props.theme.element};
+  border-radius: var(--radius);
+  box-shadow: var(--shadow);
+  margin: 0 15.9rem 0 1.6rem;
+  padding: 1.6rem 2.4rem;
+  width: 20rem;
+  position: absolute;
+  top: 5.3rem;
+  z-index: 10;
+`
+
+export const StyledRegion = styled.li`
+  color: ${(props) => props.theme.text};
+  cursor: pointer;
+  list-style: none;
+
+  @media screen and (min-width: 768px) {
+    font-size: var(--mdFont);
+  }
+
+  &:not(:last-child) {
+    margin-bottom: 0.8rem;
+  }
+`
+
+const FilterRegion = ({ selectedRegion }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleDropdown = (e) => {
+    const toggler = document.querySelector('.icon')
+
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <StyledFilterContainer>
+      <StyledTopContainer className='toggleBox' onClick={toggleDropdown}>
+        <StyledText>Filter by Region</StyledText>
+        <FontAwesomeIcon
+          icon={!isOpen ? 'chevron-down' : 'chevron-up'}
+          className='icon'
+        />
+      </StyledTopContainer>
+      {!isOpen ? null : (
+        <StyledDropdown>
+          <StyledRegion onClick={selectedRegion}>Africa</StyledRegion>
+          <StyledRegion onClick={selectedRegion}>America</StyledRegion>
+          <StyledRegion onClick={selectedRegion}>Asia</StyledRegion>
+          <StyledRegion onClick={selectedRegion}>Europe</StyledRegion>
+          <StyledRegion onClick={selectedRegion}>Oceania</StyledRegion>
+        </StyledDropdown>
+      )}
+    </StyledFilterContainer>
+  )
+}
+
+export default FilterRegion
---
+++ src/components/SearchBar.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styled from 'styled-components'
+
+export const StyledSearchBar = styled.form`
+  border-radius: var(--radius);
+  box-shadow: var(--shadow);
+  margin: 0 1.6rem 4rem;
+  position: relative;
+
+  @media screen and (min-width: 480px) {
+    width: 343px;
+  }
+
+  @media screen and (min-width: 768px) {
+    margin: 0 4rem 4rem;
+  }
+
+  @media screen and (min-width: 992px) {
+    width: 480px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    margin-left: 8rem;
+  }
+
+  .icon {
+    color: ${(props) => props.theme.input};
+    font-size: 15.56px;
+    position: absolute;
+    left: 3.2rem;
+    top: 1.6rem;
+  }
+`
+
+export const StyledInput = styled.input`
+  background-color: ${(props) => props.theme.element};
+  border: none;
+  border-radius: var(--radius);
+  color: ${(props) => props.theme.input};
+  font-family: var(--font);
+  font-size: var(--smFont);
+  height: 4.8rem;
+  outline: none;
+  padding: 1.6rem 3.2rem 1.6rem 7.4rem; // !FIXME
+  width: 100%;
+
+  @media screen and (min-width: 768px) {
+    font-size: var(--mdFont);
+  }
+
+  ::placeholder {
+    color: ${(props) => props.theme.input};
+
+    @media screen and (min-width: 768px) {
+      font-size: var(--mdFont);
+    }
+  }
+`
+
+const SearchBar = ({ searchCountry }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <StyledSearchBar onSubmit={handleSubmit}>
+      <FontAwesomeIcon icon='search' className='icon' />
+      <StyledInput
+        placeholder='Search for a country...'
+        onChange={searchCountry}
+      />
+    </StyledSearchBar>
+  )
+}
+
+export default SearchBar
