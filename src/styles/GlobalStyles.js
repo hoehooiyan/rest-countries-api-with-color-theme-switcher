e64668ad3bca6303f9572355738d2089ec53d977
hoehooiyan <hoehooiyan@gmail.com>
2020-05-16 19:00:44 +0800

    :art: Update global styles

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -4,8 +4,11 @@ export default createGlobalStyle`
   :root {
     /* TYPOGRAPHY */
     --font: 'Nunito Sans', sans-serif;
-    --smallFont: 1.4rem;
-    --regularFont: 1.6rem;
+    --xsFont: 1.2rem;
+    --smFont: 1.4rem;
+    --mdFont: 1.6rem;
+    --lgFont: 1.8rem;
+    --xlFont: 2.4rem;
     --light: 300;
     --normal: 400;
     --semi: 600;
@@ -17,6 +20,10 @@ export default createGlobalStyle`
     --md: 768px;
     --lg: 992px;
     --xl: 1440px;
+
+    /* OTHER */
+    --shadow: 0 0 6px rgba(0, 0, 0, 0.1);
+    --radius: 5px;
   }
 
   /* CSS RESET */
@@ -34,8 +41,7 @@ export default createGlobalStyle`
   body {
     background-color: ${(props) => props.theme.background};
     font-family: var(--font);
-    font-size: var(--regular-font);
+    font-size: var(--smFont);
     margin: 0 auto;
-    max-width: var(--xl);
   }
 `
