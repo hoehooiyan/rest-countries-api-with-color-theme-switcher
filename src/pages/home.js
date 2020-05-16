f2592f17dedbd59e1ec3bc9f1d5ceee329285e46
hoehooiyan <hoehooiyan@gmail.com>
2020-05-16 18:59:18 +0800

    :art: Update home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 import Layout from '../components/Layout'
+import Loader from '../components/Loader'
 import SearchBar from '../components/SearchBar'
 import FilterRegion from '../components/FilterRegion'
+import CountryContainer from '../components/CountryContainer'
 import Country from '../components/Country'
+import { ShowCurrent, NextPage, PreviousPage } from '../components/Pagination'
 import api from '../api'
 
 const StyledOuterContainer = styled.div`
@@ -17,22 +20,71 @@ const StyledContainer = styled.div`
   margin: 0 auto;
   max-width: var(--xl);
 
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: 650px) {
     flex-direction: row;
     justify-content: space-between;
-
     margin: 0 auto;
   }
 `
 
-const StyledCountriesWrapper = styled.main`
+const StyledPaginationContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
+  margin: 4rem 2rem;
+`
+
+const StyledNavigateButton = styled.div`
+  display: flex;
+  margin: 0 auto;
 `
 
 const Home = () => {
   const [countries, setCountries] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+
+  /**
+   * @reference: Pagination in React
+   * https://codepen.io/PiotrBerebecki/pen/pEYPbY?editors=0010
+   */
+  const countryPerPage = 8
+  const totalPages = Math.ceil(countries.length / countryPerPage)
+  const isFirst = 1
+  const isLast = totalPages
+
+  // logic for displaying current countries
+  const indexOfLastCountry = currentPage * countryPerPage
+  const indexOfFirstCountry = indexOfLastCountry - countryPerPage
+  const currentCountries = countries.slice(
+    indexOfFirstCountry,
+    indexOfLastCountry
+  )
+
+  const renderCountries = currentCountries.map(
+    ({ flag, name, population, region, capital }, i) => {
+      return (
+        <Country
+          key={i}
+          flag={flag}
+          name={name}
+          population={population}
+          region={region}
+          capital={capital}
+        />
+      )
+    }
+  )
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1)
+  }
+
+  const handlePreviousPage = () => {
+    setCurrentPage(currentPage - 1)
+  }
+
+  // ?fields=flag;name;population;region;capital;nativeName;subRegion;topLevelDomain;currencies;languages;borders
 
   // make an api call to get all the countries data
   useEffect(() => {
@@ -51,24 +103,27 @@ const Home = () => {
           <SearchBar />
           <FilterRegion />
         </StyledContainer>
-      </StyledOuterContainer>
 
-      <StyledCountriesWrapper>
-        {countries.length === 0 && <p>Loading results...</p>}
-        {countries.length !== 0 &&
-          countries.map((country, i) => {
-            return (
-              <Country
-                key={i}
-                flag={country.flag}
-                name={country.name}
-                population={country.population}
-                region={country.region}
-                capital={country.capital}
-              />
-            )
-          })}
-      </StyledCountriesWrapper>
+        <CountryContainer>
+          {countries.length === 0 && <Loader />}
+          {countries.length !== 0 && renderCountries}
+          {/* {countries.length !== 0 && console.log(countries)} */}
+        </CountryContainer>
+
+        {countries.length !== 0 && (
+          <StyledPaginationContainer>
+            <ShowCurrent current={currentPage} total={totalPages} />
+            <StyledNavigateButton>
+              {currentPage === isFirst ? null : (
+                <PreviousPage handlePreviousPage={handlePreviousPage} />
+              )}
+              {currentPage === isLast ? null : (
+                <NextPage handleNextPage={handleNextPage} />
+              )}
+            </StyledNavigateButton>
+          </StyledPaginationContainer>
+        )}
+      </StyledOuterContainer>
     </Layout>
   )
 }
