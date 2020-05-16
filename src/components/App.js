98dc530c7da93890d487bca0a045ec6a2540f666
hoehooiyan <hoehooiyan@gmail.com>
2020-05-16 18:58:32 +0800

    :art: Update route

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from '../pages/home'
+import NotFound from '../pages/404'
 
 const App = () => {
   return (
     <Switch>
-      <Route exact path='/'>
-        <Home />
-      </Route>
+      <Route exact path='/' component={Home} />
+      <Route component={NotFound} />
     </Switch>
   )
 }
