d29951afd3e3c3f1f942d2bea8b2563d3e1d889a
hoehooiyan <hoehooiyan@gmail.com>
2020-05-16 15:31:27 +0800

    :sparkles: Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledFooter = styled.footer`
+  width: 100%;
+`
+
+const StyledText = styled.p`
+  color: ${(props) => props.theme.text};
+  font-size: var(--xsFont);
+  font-weight: var(--extra);
+  margin: 2rem;
+  text-align: center;
+  text-transform: lowercase;
+
+  a {
+    color: ${(props) => props.theme.text};
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+`
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <StyledText>
+        Made with ❤ by <a href='https://twitter.com/hooiyancodes'>Hooi Yan</a>
+      </StyledText>
+    </StyledFooter>
+  )
+}
+
+export default Footer
